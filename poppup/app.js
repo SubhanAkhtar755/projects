dbd3861049e6f40d74e9dd060afa711803b65629
superiorcollege740@gmail.com <superiorcollege740@gmail.com>
2025-01-18 23:55:45 +0500

    add popup project

---
+++ poppup/app.js
@@ -0,0 +1,10 @@
+let poppup = document.querySelector("#poppup");
+
+// Function to display popup
+
+function openpoppup() {
+  poppup.classList.add("open-poppup")
+}
+function closepoppup() {
+    poppup.classList.remove("open-poppup")
+  }
